baa6903575163b2d1200f32b49d5bbd073170a2d
Pratik Pritam Rout <routpratik321@gmail.com>
2026-02-15 11:10:59 +0530

    chat-app project created first commit

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+import { io, app, server } from './lib/socket.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+
+server.listen(PORT, () => {
+  console.log("Server is running on port : ", PORT);
+  connectDB();
+});
